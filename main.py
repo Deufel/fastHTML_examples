c19975480996184178d749bae4a74e323c7523ff
Deufel <MDeufel13@gmail.com>
2024-10-30 13:23:48 -0500

    layout test

--- main.py
+++ main.py
@@ -2,12 +2,36 @@ from fasthtml.common import *
 import os
 from starlette.responses import PlainTextResponse  # Add this import
 
+css_scope_inline_script = Script(r'''
+window.cssScopeCount ??= 1
+window.cssScope ??= new MutationObserver(mutations => {
+    document?.body?.querySelectorAll('style:not([ready])').forEach(node => {
+        var scope = 'me__'+(window.cssScopeCount++)
+        node.parentNode.classList.add(scope)
+        node.textContent = node.textContent
+        .replace(/(?:^|\.|(\s|[^a-zA-Z0-9\-\_]))(me|this|self)(?![a-zA-Z])/g, '$1.'+scope)
+        .replace(/((@keyframes|animation:|animation-name:)[^{};]*)\.me__/g, '$1me__')
+        .replace(/(?:@media)\s(xs-|sm-|md-|lg-|xl-|sm|md|lg|xl|xx)/g,
+            (match, part1) => { return '@media '+({'sm':'(min-width: 640px)','md':'(min-width: 768px)', 'lg':'(min-width: 1024px)', 'xl':'(min-width: 1280px)', 'xx':'(min-width: 1536px)', 'xs-':'(max-width: 639px)', 'sm-':'(max-width: 767px)', 'md-':'(max-width: 1023px)', 'lg-':'(max-width: 1279px)', 'xl-':'(max-width: 1535px)'}[part1]) }
+        )
+        node.setAttribute('ready', '')
+    })
+}).observe(document.documentElement, {childList: true, subtree: true})
+''')
+
+# CSS Debugging
+css_debug = Style('*, *::before, *::after {box-sizing: border-box; outline:1px solid lime;}')
+
+
+hdrs=(css_scope_inline_script,css_debug)
+
+
 exception_handlers={
     404: lambda req, exc: Titled("404: I don't exist!"),
     418: lambda req, exc: Titled("418: I'm a teapot!")
 }
 
-app,rt = fast_app(pico=True,exception_handlers=exception_handlers)
+app,rt = fast_app(pico=True,exception_handlers=exception_handlers, hdrs=hdrs)
 
 # Add health check route
 @rt("/health")
@@ -15,30 +39,17 @@ def get():
     # Return plain text "OK" with 200 status code
     return PlainTextResponse("OK", status_code=200)
 
-def create_container_demo(is_fluid: bool = False):
-    return Main(
-            Article(
-                Header(Hgroup(
-                    H1("Pico CSS Container Width Demo"),
-                    Small("Statefull Component Toggle between fixed and fluid width layouts"))),
-                
-                Label(Input(type="checkbox", role="switch", checked=is_fluid, hx_get=f"/?fluid={not is_fluid}",
-                            hx_target="#layout-container", hx_swap="outerHTML"), 'Full Width'),
-                
-                H3("This content area will resize based on the container type:"),
-                Ul(Li("Currently using: ", Strong("Fluid Width" if is_fluid else "Fixed Width")),
-                   Li(Code('Main()')," with  class: ", Kbd(".container-fluid" if is_fluid else ".container")),
-                   Li("Try resizing your browser window to see the difference!")),
-                     
-                Footer(A('Open Pico Documents in new tab', href="https://picocss.com/docs/container", target="_blank",rel="noopener noreferrer"))
-            ),
-        id="layout-container",
-        cls="container-fluid" if is_fluid else "container"
+def layout():
+    return (
+        Title('Layout Test Builder'),
+        Main(Article('main'))
     )
 
-@rt("/")
-def get(fluid: bool = False):
-    return create_container_demo(fluid)
+# Home Page
+@rt('/')
+def get():
+    return layout()
+
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 5001))
